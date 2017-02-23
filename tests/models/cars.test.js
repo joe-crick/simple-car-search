91fc23f72a718023ce188f64caa9586061367045
Joe Crick <joe@bitovi.com>
2017-02-23 08:00:10 +0100

    (master): Test stub

---
+++ tests/models/cars.test.js
@@ -0,0 +1,15 @@
+import test from 'tape';
+import * as cars from '../../src/models/cars';
+
+function setUp() {
+
+}
+
+test('Cars ', nest => {
+  nest.test('Gets a List of Cars', assert => {
+    const data = setUp();
+    assert.ok(false, 'Returns a list of cars');
+    assert.end();
+  });
+
+});
